590c28708743d8aee14e00018e9a7e0ef659d862
MacBookPro <macbookpro@MacBookProui-MacBook-Pro.local>
2018-04-05 20:15:24 +0900

    채팅 시간 라벨 생성 및 시간 데이터 변경

--- DatingApp/View/UserCell.swift
+++ DatingApp/View/UserCell.swift
@@ -13,13 +13,13 @@ class UserCell: UITableViewCell {
     
     var message:Message?{
         didSet{
-            print(message?.toId)
+            
             if let toId = message?.toId{
                 let ref = Database.database().reference().child("users").child(toId)
                 ref.observeSingleEvent(of: .value, with: { (snap) in
                     if let dictionary = snap.value as? [String:AnyObject]{
                         self.textLabel?.text = dictionary["name"] as? String
-                        print("이름 - \(self.textLabel?.text)")
+                       
                         if let profileImageUrl = dictionary["profileImageUrl"] as? String{
                             self.profileImageView.loadImageUsingCacheWithUrlString(profileImageUrl)
                         }
@@ -27,8 +27,15 @@ class UserCell: UITableViewCell {
                 })
             }
             self.detailTextLabel?.text = message?.text
-             print("디테일 - \(self.detailTextLabel?.text)")
-            //self.timeLabel.text = message?.timestamp
+            //데이터 포멧 변경 해주기
+            if let seconds = message?.timestamp?.doubleValue {
+                let timestampDate = Date(timeIntervalSince1970: seconds)
+                
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "hh:mm:ss a"
+                timeLabel.text = dateFormatter.string(from: timestampDate)
+            }
+            
         }
     }
     
@@ -42,14 +49,31 @@ class UserCell: UITableViewCell {
         return imageView
     }()
     
+    //시간 표시 라벨
+    let timeLabel: UILabel = {
+        
+        let label = UILabel()
+        label.text = "hh:mm:ss"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = UIColor.lightGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
         addSubview(profileImageView)
-        
+        addSubview(timeLabel)
         profileImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
         profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
         profileImageView.widthAnchor.constraint(equalToConstant: 48).isActive = true
         profileImageView.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        
+        //시간 표시 라벨 크기 및 위치
+        timeLabel.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        timeLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 18).isActive = true
+        timeLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        timeLabel.heightAnchor.constraint(equalTo: (textLabel?.heightAnchor)!).isActive = true
     }
     
     override func layoutSubviews() {
