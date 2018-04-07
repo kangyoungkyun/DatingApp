2f15447c842a3437a879bd80e0ec816832627321
MacBookPro <macbookpro@MacBookProui-MacBook-Pro.local>
2018-04-07 12:11:08 +0900

    채팅 로그 collectionviewcell 커스터마이징

---
+++ DatingApp/View/ChatMessageCell.swift
@@ -0,0 +1,13 @@
+//
+//  ChatMessageCell.swift
+//  DatingApp
+//
+//  Created by MacBookPro on 2018. 4. 7..
+//  Copyright © 2018년 MacBookPro. All rights reserved.
+//
+
+import UIKit
+
+class ChatMessageCell: UICollectionViewCell {
+    
+}
